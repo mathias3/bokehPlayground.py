5438bbdfb4c10406c9d5c836eb9d49bae9ff7b0b
mathias3 <maciej.osowski@gmail.com>
2016-11-30 14:05:18 +0000

    Update fertility.py

--- fertility.py
+++ fertility.py
@@ -7,8 +7,12 @@ p.circle(fertility_latinamerica, female_literacy_latinamerica)
 # Add an x glyph to the figure p
 p.x(fertility_africa, female_literacy_africa)
 
+# Add a red circle glyph to the figure p
+p.circle(fertility_africa, female_literacy_africa, color='red', size=10, alpha=0.8)
+
 # Specify the name of the file
-output_file('fert_lit_separate.html')
+output_file('fert_lit_separate_colors.html')
+
 
 # Display the plot
 show(p)
